14fc1036cfb0c62d99bf7bfab6558dfb2ef0ccb4
Shri <er.shrijandra@yahoo.com>
2026-04-21 20:44:41 -0500

    Update app.js test3

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello from OpenShift via GitHub Actions 🚀test2\n");
+  res.end("Hello from OpenShift via GitHub Actions 🚀test3\n");
 });
 
 server.listen(PORT, () => {
